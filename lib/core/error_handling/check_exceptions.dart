188aa8ddc41e0795325110ef7022f515bb42c2f0
Jalil Teymoori <engineer.teymoori@gmail.com>
2025-09-15 01:33:47 +0330

    edit error handling exceptions

--- lib/core/error_handling/check_exceptions.dart
+++ lib/core/error_handling/check_exceptions.dart
@@ -4,53 +4,55 @@ import '../resources/data_state.dart';
 class CheckExceptions {
   CheckExceptions(Object? error);
 
-  static Future<DataState> response(Response? response) async {
+  static Future<DataFailed<T>> response<T>(Response? response) async {
     switch (response?.statusCode ?? -1) {
       case 400:
-        return DataFailed(response?.data['message'] ?? "Bad request.");
+        return DataFailed<T>(response?.data['message'] ?? "Bad request.");
       case 401:
-        return DataFailed(response?.data['message'] ?? "Unauthorized. Please log in again.");
+        return DataFailed<T>(response?.data['message'] ?? "Unauthorized. Please log in again.");
       case 403:
-        return DataFailed(response?.data['message'] ?? "Forbidden request.");
+        return DataFailed<T>(response?.data['message'] ?? "Forbidden request.");
+      case 422:
+        return DataFailed<T>(response?.data['message'] ?? "Unprocessable entity.");
       case 404:
-        return DataFailed(response?.data['message'] ?? "Resource not found.");
+        return DataFailed<T>(response?.data['message'] ?? "Resource not found.");
       case 500:
-        return DataFailed(response?.data['message'] ?? "Server error. Please try again later.");
+        return DataFailed<T>(response?.data['message'] ?? "Server error. Please try again later.");
       default:
         if (response != null) {
-          return DataFailed(
+          return DataFailed<T>(
             response.data is Map && response.data.containsKey("message")
                 ? response.data["message"]
                 : "Unexpected error occurred!",
           );
         } else {
-          return const DataFailed("No response received from server.");
+          return  DataFailed<T>("No response received from server.");
         }
     }
   }
 
-  static Future<DataState> getError(Object error) async {
+  static Future<DataState<T>> getError<T>(Object error) async {
     if (error is DioException) {
       switch (error.type) {
         case DioExceptionType.connectionTimeout:
-          return const DataFailed("Connection timeout. Please try again.");
+          return  DataFailed<T>("Connection timeout. Please try again.");
         case DioExceptionType.sendTimeout:
-          return const DataFailed("Send timeout. Please try again.");
+          return  DataFailed<T>("Send timeout. Please try again.");
         case DioExceptionType.receiveTimeout:
-          return const DataFailed("Receive timeout. Please try again.");
+          return  DataFailed<T>("Receive timeout. Please try again.");
         case DioExceptionType.badCertificate:
-          return const DataFailed("Bad SSL certificate.");
+          return  DataFailed<T>("Bad SSL certificate.");
         case DioExceptionType.cancel:
-          return const DataFailed("Request was cancelled.");
+          return  DataFailed<T>("Request was cancelled.");
         case DioExceptionType.connectionError:
-          return const DataFailed("No internet connection.");
+          return  DataFailed<T>("No internet connection.");
         case DioExceptionType.badResponse:
-          return response(error.response);
+          return response<T>(error.response);
         default:
-          return const DataFailed("Unexpected network error.");
+          return  DataFailed<T>("Unexpected network error.");
       }
     } else {
-      return const DataFailed("An unknown error occurred.");
+      return  DataFailed<T>("An unknown error occurred.");
     }
   }
 }
