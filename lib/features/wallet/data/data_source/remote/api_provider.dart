22c9e3e4faaab95871db4dd718480c0e54de2f6d
Jalil Teymoori <engineer.teymoori@gmail.com>
2025-09-15 17:42:16 +0330

    get dio from locator

--- lib/features/wallet/data/data_source/remote/api_provider.dart
+++ lib/features/wallet/data/data_source/remote/api_provider.dart
@@ -1,8 +1,9 @@
 import 'package:dio/dio.dart';
 import 'package:ton_keeper/core/utils/constants.dart';
+import 'package:ton_keeper/locator.dart';
 
 class ApiProvider{
-  final Dio _dio = Dio();
+  final Dio _dio = locator.get();
 
   Future<dynamic> getWalletData() async{
     final response =await _dio.get(Constants.walletBaseUrl , queryParameters: {
